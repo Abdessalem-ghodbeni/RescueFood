aa90e6e1cb68a82ed34152c16f2a17f96ac7b5dc
@MahaMessaoud <maaha.messaoud@esprit.tn>
2024-09-22 16:24:29 +0100

    first commit g livraison et g trajet

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AssociationController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\livraisonController;
 use App\Http\Controllers\LivreurController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestorantController;
+use App\Http\Controllers\TrajetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,9 +43,28 @@ Route::middleware(['admin'])->group(function () {
     Route::get('admin/adminDashboard', [AdminController::class, 'index'])->name('admin.index');
     // Ajoute d'autres routes d'administration ici
 });
+// Affiche la liste des livraisons
+Route::get('livraisons/dahboard', [LivraisonController::class, 'index'])->name('livraisons.index');
 
+// Affiche le formulaire de création
+Route::get('livraisons/create', [LivraisonController::class, 'create'])->name('livraisons.create');
+
+// Stocke une nouvelle livraison
+Route::post('livraisons', [LivraisonController::class, 'store'])->name('livraisons.store');
+
+// Affiche une livraison spécifique
+Route::get('livraisons/{livraison}', [livraisonController::class, 'show'])->name('livraisons.show');
+
+// Affiche le formulaire d'édition
+Route::get('livraisons/{id}/edit', [LivraisonController::class, 'edit'])->name('livraisons.edit');
+Route::put('livraisons/{livraison}', [livraisonController::class, 'update'])->name('livraisons.update');
+
+// Supprime une livraison
+Route::delete('/livraisons/{id}', [LivraisonController::class, 'destroy'])->name('livraisons.destroy');
+/*********************Trajet */
+Route::resource('trajets', TrajetController::class);
+/********************Livreur */
 Route::get('livreur/dahboard', [LivreurController::class, 'index'])->name('livreur.index');
-
 Route::get('restorant/dahboard', [RestorantController::class, 'index'])->name('restorant.index');
 
 Route::get('association/dahboard', [AssociationController::class, 'index'])->name('association.index');
