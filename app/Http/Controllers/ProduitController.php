1cc67cbe658e931eb49e9187617830254383d4b6
wiem hamdoun <wiem.hamdoun@esprit.tn>
2024-10-20 21:04:20 +0100

    v3

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -14,7 +14,7 @@ class ProduitController extends Controller
     public function index()
     {
         $produits = Produit::with('stock')->get();
-        return view ('produit.produitDashboard')->with('produits', $produits);
+        return view('produit.produitDashboard')->with('produits', $produits);
     }
 
     /**
@@ -22,7 +22,7 @@ class ProduitController extends Controller
      */
     public function create()
     {
-        $stocks = Stock::all(); 
+        $stocks = Stock::all();
         return view('produit.create', compact('stocks'));
     }
 
@@ -35,42 +35,43 @@ class ProduitController extends Controller
     //     Produit::create($input);
     //     return redirect('produit')->with('flash_message', 'Produit Addedd!');  
     // }
-    
+
     public function store(Request $request)
-    {$request->validate([
-        'nom_produit' => 'required|string|min:3',
-        'type' => 'required|string|min:3',
-        'date_expiration' => '',
-        'stock_id' => 'required|exists:stocks,id',
-    ]);
-       
+    {
+        $request->validate([
+            'nom_produit' => 'required|string|min:3',
+            'type' => 'required|string|min:3',
+            'date_expiration' => 'nullable|date|after:today',
+            'stock_id' => 'required|exists:stocks,id',
+        ]);
+
         Produit::create($request->all());
-        return redirect('produit')->with('success', 'Stock ajouté avec succès.'); 
+        return redirect('produit')->with('success', 'Stock ajouté avec succès.');
     }
-    
+
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
         $produit = Produit::find($id);
-        $stock = $produit->stock; 
+        $stock = $produit->stock;
         return view('produit.show', compact('produit', 'stock'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-     
-     public function edit(string $id)
-     {
-         $produit = Produit::find($id);
-         $stocks = Stock::all();
-         return view('produit.edit', compact('produit', 'stocks'));
-     }
-      /**
-      * Update the specified resource in storage.
-      */
+
+    public function edit(string $id)
+    {
+        $produit = Produit::find($id);
+        $stocks = Stock::all();
+        return view('produit.edit', compact('produit', 'stocks'));
+    }
+    /**
+     * Update the specified resource in storage.
+     */
     //  public function update(Request $request, $id)
     //  {
     //      $produit = Produit::find($id);
@@ -83,19 +84,19 @@ class ProduitController extends Controller
         $request->validate([
             'nom_produit' => 'required|string|min:3',
             'type' => 'required|string|min:3',
-            'date_expiration' => '',
+            'date_expiration' => 'nullable|date|after:today',
             'stock_id' => 'required|exists:stocks,id',
         ]);
         $produit = Produit::findOrFail($id);
         $produit->update($request->all());
-        return redirect('produit')->with('flash_message', 'Produit Updated!');  
+        return redirect('produit')->with('flash_message', 'Produit Updated!');
     }
-     /**
-      * Remove the specified resource from storage.
-      */
-     public function destroy(string $id)
-     {
-         Produit::destroy($id);
-         return redirect('produit')->with('flash_message', 'Produit deleted!');  
-     }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Produit::destroy($id);
+        return redirect('produit')->with('flash_message', 'Produit deleted!');
+    }
 }
